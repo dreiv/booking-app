f6c605e8fd3c258350930efbbd02315367b97f39
Voicu Andrei <dreivoicu@gmail.com>
2026-04-20 21:27:54 +0300

    fix env naming

--- client/src/core/services/http.ts
+++ client/src/core/services/http.ts
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_BACKEND_URL || "";
+const BASE_URL = import.meta.env.VITE_API_URL || "";
 
 interface HttpOptions extends RequestInit {
   params?: Record<string, string>;
