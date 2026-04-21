f53ff3668c0ad9827b9fa3e3f363201ff4fbe5f6
Voicu Andrei <dreivoicu@gmail.com>
2026-04-21 11:46:42 +0300

    add icon library, test lint rules

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,31 +1,27 @@
-import js from "@eslint/js";
-import oxlint from "eslint-plugin-oxlint";
-import reactCompiler from "eslint-plugin-react-compiler";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
-import storybook from "eslint-plugin-storybook";
-import globals from "globals";
-import tseslint from "typescript-eslint";
+import js from '@eslint/js'
+import pluginVitest from '@vitest/eslint-plugin'
+import oxlint from 'eslint-plugin-oxlint'
+import reactCompiler from 'eslint-plugin-react-compiler'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import storybook from 'eslint-plugin-storybook'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   {
-    ignores: [
-      "dist/**",
-      "coverage/**",
-      "storybook-static/**",
-      "node_modules/**",
-    ],
+    ignores: ['dist/**', 'coverage/**', 'storybook-static/**', 'node_modules/**'],
   },
 
   js.configs.recommended,
   ...tseslint.configs.recommended,
 
   {
-    files: ["**/*.{ts,tsx}"],
+    files: ['**/*.{ts,tsx}'],
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
-      "react-compiler": reactCompiler,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'react-compiler': reactCompiler,
     },
     languageOptions: {
       ecmaVersion: 2020,
@@ -36,14 +32,23 @@ export default tseslint.config(
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
-      "react-compiler/react-compiler": "error",
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+      'react-compiler/react-compiler': 'error',
     },
   },
 
-  ...storybook.configs["flat/recommended"],
-  ...oxlint.configs["flat/recommended"], // Oxlint - ALWAYS LAST
-);
+  {
+    files: ['**/*.test.tsx', '**/*.spec.ts'],
+    plugins: {
+      'testing-library': testingLibraryPlugin,
+      vitest: vitestPlugin,
+    },
+    rules: {
+      ...testingLibraryPlugin.configs.dom.rules,
+      ...pluginVitest.configs.recommended.rules,
+    },
+  },
+
+  ...storybook.configs['flat/recommended'],
+  ...oxlint.configs['flat/recommended'], // Oxlint - ALWAYS LAST
+)
